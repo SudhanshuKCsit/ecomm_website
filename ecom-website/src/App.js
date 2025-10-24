cf6278efa05dd0c8686c7bc3429b2ffd0c477468
Sudhanshu Kumar <sudhanshukumarsingh33@gmail.com>
2025-10-24 14:58:39 +0530

    first commit

---
+++ ecom-website/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+import ProductList from "./components/products/ProductList";
+const App =() =>{
+  return (
+    <div>
+      <Header />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
